c7e24e2c58b7bf5b0e942108816a6072d8133143
manantrimbakey <manantrimbakey@github.com>
2021-05-10 21:30:39 +0530

    handled for line numbers

--- src/main/java/com/manan/org/events/LIMIT_USAGE_FOR_NSProcessor.java
+++ src/main/java/com/manan/org/events/LIMIT_USAGE_FOR_NSProcessor.java
@@ -17,7 +17,7 @@ public class LIMIT_USAGE_FOR_NSProcessor extends EventProcessorClass {
                 Matcher v = DebugAnalyser.masterPattern.matcher(lines);
                 boolean b = v.matches();
                 if (b) {
-                    DebugAnalyser.addEntryObject(generator, event, text.toString());
+                    DebugAnalyser.addEntryObject(generator, event, text.toString(),0);
                     app.process(lines, scanner);
                     return;
                 } else {
